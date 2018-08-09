550f6489ba688cf28010e63073ae222122069d75
chaewonmoon <chaewonmoon77@gmail.com>
2018-08-09 11:01:19 +0900

    first commit

---
+++ src/main/java/com/MHE_Project/MheProjectApplication.java
@@ -0,0 +1,37 @@
+package com.MHE_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+/**
+ * 
+ * created by @author 문채원 on July,2018
+ *
+ *
+ **/
+
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.MHE_Project.web", "com.MHE_Project.domain"})
+@EnableAutoConfiguration
+public class MheProjectApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MheProjectApplication.class, args);
+	}
+	/*
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+			resolver.setPrefix("WEB-INF/view");
+			resolver.setSuffix(".jsp");
+			return resolver;
+	}
+	*/
+}
---
+++ src/main/java/com/MHE_Project/domain/MHE_SensorDataRepository.java
@@ -0,0 +1,28 @@
+package com.MHE_Project.domain;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+ 
+
+@Repository
+public interface MHE_SensorDataRepository extends JpaRepository<MHE_SensorData, Long> {
+	
+	@Query("SELECT s FROM MHE_SensorData s WHERE s.MAC=:MAC ORDER BY s.id ASC")
+	List<MHE_SensorData> findByMAC(@Param("MAC") String q);
+	
+	@Query("SELECT s FROM MHE_SensorData s WHERE s.UID=:UID ORDER BY s.id ASC")
+	List<MHE_SensorData> findByID(@Param("UID") String q2);
+	
+	@Query("SELECT s FROM MHE_SensorData s WHERE s.time=:TIME ORDER BY s.id ASC")
+	List<MHE_SensorData> findByTIME(@Param("TIME") String q3); 
+	
+	@Query("SELECT s FROM MHE_SensorData s WHERE s.time BETWEEN (:TIMEP1) AND (:TIMEP2) ORDER BY s.id ASC")
+	List<MHE_SensorData> findByPERIOD(@Param("TIMEP1") String q4, @Param("TIMEP2") String q5);
+	
+	
+}
+
